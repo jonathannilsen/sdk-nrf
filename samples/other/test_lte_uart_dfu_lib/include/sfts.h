fa09de981b5776c30a4efac20867ca23d77c178b
Grzegorz Swiderski <grzegorz.swiderski@nordicsemi.no>
2020-07-21 18:39:28 +0200

    Add a custom service for file transfer over BLE + script for preparing a file transmitter board

---
+++ samples/other/test_lte_uart_dfu_lib/include/sfts.h
@@ -0,0 +1,119 @@
+/**
+ * @file
+ * @defgroup bt_gatt_sfts BLE GATT Simple File Transfer Service API
+ * @{
+ * @brief API for the BLE GATT Simple File Transfer Service.
+ */
+
+#ifndef BT_GATT_SFTS_H_
+#define BT_GATT_SFTS_H_
+
+#include <bluetooth/uuid.h>
+#include <bluetooth/conn.h>
+#include <zephyr/types.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/** @brief UUID of the SFTS Service. **/
+#define SFTS_UUID_SERVICE \
+	BT_UUID_128_ENCODE(0xdb03a5c6, 0x86a8, 0x4c57, 0xb2ca, 0x367fbe53be0a)
+
+/** @brief UUID of the SFTS Data Characteristic. **/
+#define SFTS_UUID_DATA_CHAR \
+	BT_UUID_128_ENCODE(0xdb03a5c7, 0x86a8, 0x4c57, 0xb2ca, 0x367fbe53be0a)
+
+#define BT_UUID_SFTS_SERVICE	BT_UUID_DECLARE_128(SFTS_UUID_SERVICE)
+#define BT_UUID_SFTS_DATA_CHAR	BT_UUID_DECLARE_128(SFTS_UUID_DATA_CHAR)
+
+/** @brief Opcode for new file transfer request. */
+#define SFTS_OP_NEW		0x00
+/** @brief Opcode for data segment received. */
+#define SFTS_OP_DATA		0x01
+/** @brief Opcode for file transfer completed. */
+#define SFTS_OP_COMPLETE	0x02
+/** @brief Opcode for file transfer aborted by client. */
+#define SFTS_OP_ABORT		0xFF
+
+/** @brief Simple File Transfer Service event callbacks.
+ *
+ *  The application is responsible for validating and storing files
+ *  transferred using this service. Therefore, all callbacks are mandatory.
+ */
+struct bt_gatt_stfs_cb {
+	/** @brief Callback for new file transfer request.
+	 *
+	 *  This callback is called whenever the service client at peer
+	 *  wants to transfer a new file with a specific size. The server
+	 *  gets an opportunity to check whether it has enough resources to
+	 *  store the incoming file, and accept or reject the transfer.
+	 *
+	 *  @param conn		Pointer to associated connection object.
+	 *  @param file_size	Size of the file to be transferred.
+	 *
+	 *  @retval 0		File accepted.
+	 *  @retval -ENOMEM	File rejected because it is too big.
+	 *  @retval -ECANCELED	File rejected for some other reason.
+	 */
+	int (*new_cb)(struct bt_conn *conn, const u32_t file_size);
+
+	/** @brief Callback for data segment received.
+	 *
+	 *  This callback is called whenever the service client at peer
+	 *  sends a new part of the current file. The server has the option
+	 *  to terminate the transfer after inspecting the file contents.
+	 *  For example, the server may only allow files with a specific
+	 *  header by accepting or rejecting the first data segment.
+	 *
+	 *  @param conn		Pointer to associated connection object.
+	 *  @param data		Pointer to a data buffer.
+	 *  @param len		Length of the data in the buffer.
+	 *
+	 *  @retval 0		Data segment accepted and stored.
+	 *  @retval -ENOMEM	Not enough resources to complete transfer.
+	 *  @retval -ECANCELED	Terminate unwanted transfer.
+	 */
+	int (*data_cb)(struct bt_conn *conn, const u8_t *data, u16_t len);
+
+	/** @brief Callback for file transfer completed.
+	 *
+	 *  This callback is called whenever the service client at peer
+	 *  signals that it has finished transmitting the current file.
+	 *
+	 *  @param conn		Pointer to associated connection object.
+	 *  @param crc		CRC of the complete file for an optional check.
+	 *
+	 *  @retval 0		File transfer successful.
+	 *  @retval -EFAULT	CRC error.
+	 */
+	int (*complete_cb)(struct bt_conn *conn, const u32_t crc);
+
+	/** @brief Callback for file transfer aborted by client.
+	 *
+	 *  This callback is called whenever the service client at peer
+	 *  either sends an abort code or disconnects during a transfer.
+	 *
+	 *  @param conn		Pointer to associated connection object.
+	 */
+	void (*abort_cb)(struct bt_conn *conn);
+};
+
+/** @brief Initialize the service.
+ *
+ *  @param cb		Pointer to event callbacks.
+ *
+ *  @retval 0		Success.
+ *  @retval -EINVAL	Null pointer error.
+ */
+int bt_gatt_stfs_init(const struct bt_gatt_stfs_cb *cb);
+
+#ifdef __cplusplus
+}
+#endif
+
+/**
+ * @}
+ */
+
+#endif /* BT_GATT_SFTS_H_ */
---
+++ samples/other/test_lte_uart_dfu_lib/scripts/sfts_prep.py
@@ -0,0 +1,63 @@
+import pylink # pip install pylink-square
+from sys import argv, exit
+
+ADDR_FIRMWARE       = 0x00000
+ADDR_SFTS_SIZE      = 0x50000
+ADDR_SFTS_DATA      = 0x50008
+JLINK_TARGET_DEVICE = 'nRF52840_xxAA'
+USAGE_OPTION_VERIFY = '--verify'
+USAGE_MESSAGE       = '''
+This script will prepare an SFTS client board to transmit a file over BLE.
+Usage:
+  python {0} <firmware.hex> <data.bin>
+  python {0} {1}
+'''[1:-1]
+
+def connect():
+    print('Connecting to JLink...')
+    jlink = pylink.JLink()
+    jlink.open()
+    jlink.set_tif(pylink.enums.JLinkInterfaces.SWD)
+    jlink.connect(JLINK_TARGET_DEVICE)
+
+    return jlink
+
+if __name__ == '__main__':
+    if len(argv) == 2 and argv[1] == USAGE_OPTION_VERIFY:
+        jlink = connect()
+        bytes = jlink.memory_read8(ADDR_SFTS_SIZE, 16)
+
+        print(f'0x{ADDR_SFTS_SIZE:08x}:', *(f'{b:02x}' for b in bytes), '...')
+
+    elif len(argv) == 3:
+        jlink = connect()
+        firmware_hex, data_bin = argv[1:]
+
+        print(f'Flashing {firmware_hex:}...')
+        jlink.flash_file(firmware_hex, ADDR_FIRMWARE);
+
+        print(f'Flashing {data_bin:}...')
+        jlink.flash_file(data_bin, ADDR_SFTS_DATA);
+
+        size, crc = 0, 0xFFFFFFFF
+        with open(data_bin, 'rb') as file:
+            while True:
+                try:
+                    byte = file.read(1)[0]
+                    size += 1
+                    crc ^= byte;
+                    for _ in range(8):
+                        crc = (crc >> 1) ^ (0xEDB88320 & -(crc & 1))
+                except IndexError:
+                    break
+
+        print('Writing length and CRC...')
+        jlink.flash_write32(ADDR_SFTS_SIZE, [size, ~crc])
+
+        print('Resetting...')
+        jlink.reset(halt=False)
+
+        print('Done')
+
+    else:
+        print(USAGE_MESSAGE.format(argv[0], USAGE_OPTION_VERIFY))
