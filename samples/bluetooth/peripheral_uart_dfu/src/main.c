215b7630bb5ad0a04f02ebab7ef7edc2bc81a6a5
Grzegorz Swiderski <grzegorz.swiderski@nordicsemi.no>
2020-07-27 12:21:56 +0200

    Working DFU BLE UART project
    So far this is only a naïve solution where everything runs in BT RX thread. Proper threaded solution is WIP and will probably not compile yet.

--- samples/bluetooth/peripheral_uart_dfu/src/main.c
+++ samples/bluetooth/peripheral_uart_dfu/src/main.c
@@ -1,22 +1,60 @@
 #include <zephyr.h>
 #include <sys/crc.h>
 #include <sys/printk.h>
-#include <logging/log.h>
+#include <sys/ring_buffer.h>
 #include <string.h>
 #include <uart_dfu.h>
 #include <uart_dfu_target_server.h>
+#include <dfu/dfu_target.h>
+#include <dfu/mcuboot.h>
+
 #include <bluetooth/bluetooth.h>
 #include <bluetooth/conn.h>
 #include "sfts.h"
 
-#define RETURN_ON_ERROR(_err, _msg)			\
-if (_err) {									\
-	LOG_ERR("%s (err %d)\n", _msg, _err);	\
-	return;									\
+#define RETURN_ON_ERROR(_err, _msg)		\
+if (_err) {					\
+	printk("%s (err %d)\n", _msg, _err);	\
+	return;					\
 }
 
+/* Taken from dfu_target.c */
+#define IDENTIFY_BUF_SIZE 32
 
-static u32_t current_crc;
+/* Data buffer for identifying DFU image type. */
+static u8_t id_buf[IDENTIFY_BUF_SIZE];
+static u16_t id_buf_len;
+
+/* Context for DFU over BLE using SFTS. */
+static struct {
+	enum {
+		SFTS_DFU_STATE_IDLE,
+		SFTS_DFU_STATE_WAITING_FOR_FIRST_SEGMENT,
+		SFTS_DFU_STATE_TRANSFER_IN_PROGRESS,
+#if CONFIG_DFU_THREAD
+		SFTS_DFU_STATE_TRANSFER_ABORTING,
+#endif
+	} state;
+	struct bt_conn *conn;
+	u32_t file_size;
+	u32_t current_crc;
+#if CONFIG_DFU_THREAD
+	int img_type;
+	struct ring_buf rbuf;
+	u8_t rbuf_internal[CONFIG_DFU_THREAD_BUF_SIZE];
+	struct k_sem sem_rbuf;
+	struct k_sem sem_exit;
+	struct k_thread thread;
+	bool thread_exit;
+	k_tid_t tid;
+#endif
+} sfts_dfu_ctx;
+
+#if CONFIG_DFU_THREAD
+K_THREAD_STACK_DEFINE(dfu_thread_stack, 128);
+#endif
+
+/* BLE Advertising Data. */
 static const struct bt_data ad[] = {
 	BT_DATA_BYTES(BT_DATA_FLAGS, (BT_LE_AD_GENERAL | BT_LE_AD_NO_BREDR)),
 	BT_DATA_BYTES(BT_DATA_UUID128_ALL, SFTS_UUID_SERVICE),
@@ -24,43 +62,290 @@ static const struct bt_data ad[] = {
 
 static struct uart_dfu_target_server target_server;
 
-LOG_MODULE_REGISTER(peripheral_uart_dfu, LOG_LEVEL_DBG);
+/*****************************************************************************
+ * Static functions
+ *****************************************************************************/
 
-
-int on_sfts_new(struct bt_conn *conn, const u32_t file_size)
+static void identify_buf_append(const u8_t *data, u16_t len)
 {
-	printk("New file with size: %u\n", file_size);
-
-	current_crc = 0;
-	return 0;
+	len = MIN(len, IDENTIFY_BUF_SIZE - id_buf_len);
+	memcpy(&id_buf[id_buf_len], data, len);
 }
 
-int on_sfts_data(struct bt_conn *conn, const u8_t *data, u16_t len)
+static int image_identify(const u8_t *data, u16_t len)
 {
-	printk("Received %u bytes: ", len);
-	for (u16_t i = 0; i < len; i++) {
-		printk("%02X", data[i]);
+	int img_type;
+
+	/* Get DFU image type of the first segment.
+	 * Use identify buf if dfu_target_img_type returned -EAGAIN before.
+	 */
+	if (!id_buf_len) {
+		img_type = dfu_target_img_type(data, len);
+	} else {
+		identify_buf_append(data, len);
+		img_type = dfu_target_img_type(id_buf, id_buf_len);
 	}
-	printk("\n");
 
-	current_crc = crc32_ieee_update(current_crc, data, len);
+	if (img_type == -EAGAIN) {
+		/* More data is needed before the image can be identified.
+		 * Temporarily store first segment in identify buf.
+		 */
+		if (!id_buf_len) {
+			identify_buf_append(data, len);
+		}
+	}
+
+	id_buf_len = 0;
+	return img_type;
+}
+
+static int sfts_dfu_start(int img_type)
+{
+	int err;
+	bool reset;
+	size_t offset;
+
+	for (reset = false; ; reset = true) {
+		err = dfu_target_init(img_type, sfts_dfu_ctx.file_size, NULL);
+		if ((err < 0) && (err != -EBUSY)) {
+			(void) dfu_target_reset();
+			return err;
+		}
+
+		err = dfu_target_offset_get(&offset);
+		if (err) {
+			return err;
+		}
+
+		if (offset > 0) {
+			/* SFTS does not support starting download from offset.
+			 * Reset and reinitialize target on next iteration.
+			 */
+			if (reset) {
+				/* Target was reset on previous iteration. */
+				__ASSERT(0, "DFU target offset is "
+					    "nonzero after reset");
+				return -EFAULT;
+			}
+
+			err = dfu_target_reset();
+			if (err) {
+				return err;
+			}
+		} else {
+			/* Ready for transfer. */
+			return 0;
+		}
+	}
+}
+
+static int sfts_dfu_stop(bool successful)
+{
+	int err = dfu_target_done(successful);
+
+	/* Context reset. */
+	bt_conn_unref(sfts_dfu_ctx.conn);
+	sfts_dfu_ctx.conn = NULL;
+	sfts_dfu_ctx.state = SFTS_DFU_STATE_IDLE;
+
+	return err;
+}
+
+#if CONFIG_DFU_THREAD
+static void sfts_dfu_thread_cleanup(void)
+{
+	sfts_dfu_ctx.thread_exit = false;
+	k_sem_give(&sfts_dfu_ctx.sem_exit);
+}
+
+/**
+ * @brief DFU thread entry function.
+ * @details Used for calling dfu_target API outside of BT RX thread.
+ *
+ * @param p1	Image type passed from @a first_image_segment_in.
+ * @param p2	Unused.
+ * @param p3	Unused.
+ */
+static void sfts_dfu_thread(void *p1, void *p2, void *p3)
+{
+	int err;
+	u8_t *data;
+	u32_t len;
+
+	err = sfts_dfu_start((int) p1);
+	if (err) {
+		sfts_dfu_ctx.state = SFTS_DFU_STATE_TRANSFER_ABORTING;
+		sfts_dfu_thread_cleanup();
+		return;
+	}
+
+	while (!sfts_dfu_ctx.thread_exit) {
+		/* Wait until more data is received and copied. */
+		k_sem_take(&sfts_dfu_ctx.sem_rbuf, K_FOREVER);
+
+		for (;;) {
+			/* Fetch as much contiguous data as possible. */
+			len = ring_buf_get_claim(&sfts_dfu_ctx.rbuf, &data,
+						 CONFIG_DFU_THREAD_BUF_SIZE);
+			if (!len) {
+				break;
+			}
+
+			err = dfu_target_write(data, len);
+			if (!err) {
+				err = ring_buf_get_finish(&sfts_dfu_ctx.rbuf,
+							  len);
+				if (!err) {
+					/* Success state. */
+					k_yield();
+					continue;
+				}
+			}
+
+			/* Failure state. */
+			sfts_dfu_ctx.state = SFTS_DFU_STATE_TRANSFER_ABORTING;
+			sfts_dfu_thread_cleanup();
+			return;
+		}
+	}
+	sfts_dfu_thread_cleanup();
+}
+
+static void sfts_dfu_thread_exit(void)
+{
+	/* Set flag to signal a graceful exit out of thread loop. */
+	sfts_dfu_ctx.thread_exit = true;
+
+	/* Wake up thread in case it is waiting for semaphore. */
+	k_sem_give(&sfts_dfu_ctx.sem_rbuf);
+
+	/* Sleep until thread exits loop. */
+	/* FIXME: forever may block too long so pick a delay and return bool */
+	k_sem_take(&sfts_dfu_ctx.sem_exit, K_FOREVER);
+}
+#endif /* CONFIG_DFU_THREAD */
+
+static int next_image_segment_in(const u8_t *data, u16_t len)
+{
+#if CONFIG_DFU_THREAD
+	/* Pipe data to DFU thread. */
+	/* TODO */
+#else
+	int err = dfu_target_write(data, len);
+	if (err) {
+		return -ECANCELED;
+	}
+#endif
+
+	sfts_dfu_ctx.current_crc = crc32_ieee_update(sfts_dfu_ctx.current_crc,
+						     data, len);
 	return 0;
 }
 
-int on_sfts_complete(struct bt_conn *conn, const u32_t crc)
+static int first_image_segment_in(const u8_t *data, u16_t len)
 {
-	/* TODO should compute CRC based on what's written to flash */
-	if (current_crc == crc) {
-		printk("CRC OK - Transfer complete\n");
+	int img_type = image_identify(data, len);
+	if (img_type == -EAGAIN) {
+		/* Wait for more data. */
+		return 0;
+	} else if (img_type < 0) {
+		printk("DFU image type unknown\n");
+		return -ECANCELED;
+	}
+
+	printk("DFU image type: %d\n", img_type);
+
+#if CONFIG_DFU_THREAD
+	/* Set up a separate thread for starting DFU. */
+	sfts_dfu_ctx.tid =
+		k_thread_create(&sfts_dfu_ctx.thread, dfu_thread_stack,
+				K_THREAD_STACK_SIZEOF(dfu_thread_stack),
+				sfts_dfu_thread, (void *) img_type, NULL, NULL,
+				CONFIG_DFU_THREAD_PRIO, 0, K_NO_WAIT);
+#else
+	/* Start DFU in this thread. */
+	int err = sfts_dfu_start(img_type);
+	if (err) {
+		return -ECANCELED;
+	}
+#endif
+
+	sfts_dfu_ctx.state = SFTS_DFU_STATE_TRANSFER_IN_PROGRESS;
+	return next_image_segment_in(data, len);
+}
+
+/*****************************************************************************
+ * SFTS event callbacks
+ *****************************************************************************/
+
+static int on_sfts_new(struct bt_conn *conn, const u32_t file_size)
+{
+	__ASSERT_NO_MSG(sfts_dfu_ctx.state == SFTS_DFU_STATE_IDLE);
+	__ASSERT_NO_MSG(sfts_dfu_ctx.conn != conn);
+
+	if (sfts_dfu_ctx.conn == NULL) {
+		sfts_dfu_ctx.state = SFTS_DFU_STATE_WAITING_FOR_FIRST_SEGMENT;
+		sfts_dfu_ctx.conn = bt_conn_ref(conn);
+		sfts_dfu_ctx.file_size = file_size;
+		sfts_dfu_ctx.current_crc = 0;
+
+		printk("DFU transfer started\n");
 		return 0;
 	}
-	printk("CRC error: received %08x, computed %08x\n", crc, current_crc);
-	return -EFAULT;
+	return -ECANCELED;
 }
 
-void on_sfts_abort(struct bt_conn *conn)
+static int on_sfts_data(struct bt_conn *conn, const u8_t *data, u16_t len)
 {
-	printk("Transfer aborted\n");
+	__ASSERT_NO_MSG(sfts_dfu_ctx.conn == conn);
+
+	switch (sfts_dfu_ctx.state) {
+	case SFTS_DFU_STATE_WAITING_FOR_FIRST_SEGMENT:
+		return first_image_segment_in(data, len);
+	case SFTS_DFU_STATE_TRANSFER_IN_PROGRESS:
+		return next_image_segment_in(data, len);
+#if CONFIG_DFU_THREAD
+	case SFTS_DFU_STATE_TRANSFER_ABORTING:
+		return -ECANCELED;
+#endif
+	case SFTS_DFU_STATE_IDLE:
+	default:
+		/* Should not happen. */
+		__ASSERT_NO_MSG(0);
+		return -ECANCELED;
+	}
+}
+
+static int on_sfts_complete(struct bt_conn *conn, const u32_t crc)
+{
+	__ASSERT_NO_MSG(sfts_dfu_ctx.state != SFTS_DFU_STATE_IDLE);
+	__ASSERT_NO_MSG(sfts_dfu_ctx.conn == conn);
+
+	if (sfts_dfu_ctx.state == SFTS_DFU_STATE_TRANSFER_IN_PROGRESS &&
+	    sfts_dfu_ctx.current_crc == crc) {
+		int err = sfts_dfu_stop(true);
+		if (err) {
+			return -ECANCELED;
+		} else {
+			return 0;
+		}
+	} else {
+		(void) sfts_dfu_stop(false);
+		return -ECANCELED;
+	}
+}
+
+static void on_sfts_abort(struct bt_conn *conn)
+{
+	__ASSERT_NO_MSG(sfts_dfu_ctx.state != SFTS_DFU_STATE_IDLE);
+	__ASSERT_NO_MSG(sfts_dfu_ctx.conn == conn);
+
+#if CONFIG_DFU_THREAD
+	sfts_dfu_thread_exit();
+#endif
+
+	(void) sfts_dfu_stop(false);
+	printk("DFU transfer aborted\n");
 }
 
 static const struct bt_gatt_stfs_cb stfs_callbacks = {
@@ -70,6 +355,9 @@ static const struct bt_gatt_stfs_cb stfs_callbacks = {
 	.abort_cb = on_sfts_abort,
 };
 
+/*****************************************************************************
+ * Entry point
+ *****************************************************************************/
 
 void main(void)
 {
@@ -94,4 +382,13 @@ void main(void)
 			"Advertising failed to start");
 
 	printk("Advertising successfully started\n");
+
+#if CONFIG_DFU_THREAD
+	ring_buf_init(&sfts_dfu_ctx.rbuf, CONFIG_DFU_THREAD_BUF_SIZE,
+		      sfts_dfu_ctx.rbuf_internal);
+	k_sem_init(&sfts_dfu_ctx.sem_rbuf, 0, 1);
+	k_sem_init(&sfts_dfu_ctx.sem_exit, 0, 1);
+#endif
+
+	boot_write_img_confirmed();
 }
