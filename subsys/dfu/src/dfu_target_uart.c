800b842d38aff42587e7ed9a90de702c7291581f
Jonathan Nilsen <Jonathan.Nilsen@nordicsemi.no>
2020-08-07 09:33:55 +0200

    dfu_target_uart tests and bugfixes

--- subsys/dfu/src/dfu_target_uart.c
+++ subsys/dfu/src/dfu_target_uart.c
@@ -23,8 +23,8 @@ static const u8_t uart_header_magic[] = {
 
 static dfu_target_callback_t callback = NULL;
 static struct uart_dfu_cli cli;
-static atomic_t byte_counter = 0;
-static atomic_t initialized = 0;
+static size_t byte_counter = 0;
+static bool initialized = false;
 static int status_res = 0;
 static size_t offset_res = 0;
 K_SEM_DEFINE(dfu_target_uart_sem, 0, 1);
@@ -39,9 +39,6 @@ LOG_MODULE_REGISTER(dfu_target_uart, CONFIG_DFU_TARGET_LOG_LEVEL);
 static void cli_status_handle(int status, void * context)
 {
 	ARG_UNUSED(context);
-
-	LOG_DBG("cli status(status=%d)", status);
-
 	status_res = status;
 	k_sem_give(&dfu_target_uart_sem);
 }
@@ -49,9 +46,6 @@ static void cli_status_handle(int status, void * context)
 static void cli_offset_handle(size_t offset, void * context)
 {
 	ARG_UNUSED(context);
-
-	LOG_DBG("cli offset(offset=%u)", offset);
-	
 	status_res = 0;
 	offset_res = offset;
 	k_sem_give(&dfu_target_uart_sem);
@@ -60,10 +54,6 @@ static void cli_offset_handle(size_t offset, void * context)
 static void cli_error_handle(int error, void * context)
 {
 	ARG_UNUSED(context);
-
-	LOG_DBG("cli error(error=%d)", error);
-
-	/* XXX: Differentiate between local and remote errors? */
 	status_res = error;
 	k_sem_give(&dfu_target_uart_sem);	
 }
@@ -75,12 +65,17 @@ static int cli_init(void)
 	
 	cli_cb.status_cb = cli_status_handle;
 	cli_cb.offset_cb = cli_offset_handle;
-
 	err = uart_dfu_cli_init(&cli, &cli_cb, cli_error_handle, NULL);
 	if (err != 0) {
 		return err;
 	}
 	return uart_dfu_cli_bind(CONFIG_DFU_TARGET_UART_INSTANCE, &cli);
+}
+
+static void session_state_reset(void)
+{
+	k_sem_reset(&dfu_target_uart_sem);
+	byte_counter = 0;
 }
 
 
@@ -97,16 +92,17 @@ int dfu_target_uart_init(size_t file_size, dfu_target_callback_t cb)
 {
 	int err;
 
-	/* TODO: ensure that this is not happening at a bad time. */
-	k_sem_reset(&dfu_target_uart_sem);
-	atomic_set(&byte_counter, 0);
+	if (file_size < sizeof(uart_header_magic)) {
+		return -EINVAL;
+	}
 
-	if (!atomic_set(&initialized, 1)) {
+	session_state_reset();
+	if (!initialized) {
 		err = cli_init();
 		if (err != 0) {
-			atomic_set(&initialized, 0);
 			return err;	
 		}
+		initialized = true;
 	}
 
 	err = uart_dfu_cli_init_send(CONFIG_DFU_TARGET_UART_INSTANCE,
@@ -114,7 +110,6 @@ int dfu_target_uart_init(size_t file_size, dfu_target_callback_t cb)
 	if (err != 0) {
 		return err;
 	}
-	
 	k_sem_take(&dfu_target_uart_sem, K_FOREVER);
 	return status_res;
 }
@@ -122,6 +117,10 @@ int dfu_target_uart_init(size_t file_size, dfu_target_callback_t cb)
 int dfu_target_uart_offset_get(size_t * offset)
 {
 	int err;
+
+	if (offset == NULL) {
+		return -EINVAL;
+	}
 
 	err = uart_dfu_cli_offset_send(CONFIG_DFU_TARGET_UART_INSTANCE);
 	if (err != 0) {
@@ -137,17 +136,15 @@ int dfu_target_uart_offset_get(size_t * offset)
 int dfu_target_uart_write(const void * const buf, size_t len)
 {
 	int err;
-	int count;
 	u8_t * data;
 	size_t data_len;
 
 	/* Check that len will not cause an overflow of the counter. */
-	if (len > INT32_MAX) {
+	if (buf == NULL || len > INT32_MAX) {
 		return -EINVAL;
 	}
 
-	count = atomic_add(&byte_counter, (atomic_t) len);
-	if (count < sizeof(uart_header_magic)) {
+	if (byte_counter < sizeof(uart_header_magic)) {
 		/* The magic number is stripped off the header
 		   before transmission. */
 		if (len > sizeof(uart_header_magic)) {
@@ -156,6 +153,7 @@ int dfu_target_uart_write(const void * const buf, size_t len)
 		} else {
 			/* The entirety of the buffer was stripped off;
 			   return without transmitting. */
+			byte_counter = len;
 			return 0;
 		}
 	} else {
@@ -169,7 +167,8 @@ int dfu_target_uart_write(const void * const buf, size_t len)
 	if (err != 0) {
 		return err;
 	}
-
+	byte_counter += len;
+	
 	k_sem_take(&dfu_target_uart_sem, K_FOREVER);
 	return status_res;
 }
@@ -178,24 +177,18 @@ int dfu_target_uart_done(bool successful)
 {
 	int err;
 
-	/* TODO: better handling of cases where this is called at weird times.
-			 e.g. call uart_dfu_cli_stop() if necessary. */
-
-	err = uart_dfu_cli_done_send(CONFIG_DFU_TARGET_UART_INSTANCE,
-				     successful);
-	if (err != 0) {
-		goto cleanup;
-	}
-
-	k_sem_take(&dfu_target_uart_sem, K_FOREVER);
-	err = status_res;
-
-cleanup:
 	err = uart_dfu_cli_stop(CONFIG_DFU_TARGET_UART_INSTANCE);
 	if (err == -EINPROGRESS) {
 		k_sem_take(&dfu_target_uart_sem, K_FOREVER);
 	}
-	k_sem_reset(&dfu_target_uart_sem);
-	atomic_set(&byte_counter, 0);
-	return 0;
+	
+	err = uart_dfu_cli_done_send(CONFIG_DFU_TARGET_UART_INSTANCE,
+				     successful);
+	if (err == 0) {
+		k_sem_take(&dfu_target_uart_sem, K_FOREVER);
+		err = status_res;
+	}
+
+	session_state_reset();
+	return err;
 }
--- tests/subsys/dfu/dfu_target_uart/src/main.c
+++ tests/subsys/dfu/dfu_target_uart/src/main.c
@@ -11,6 +11,9 @@
 #include <dfu_target_uart.h>
 #include <uart_dfu.h>
 
+
+#define TEST_FILE_SIZE 		(150 * 1024)
+
 #define UART_DFU_HEADER_LENGTH	4
 #define QUEUE_PRIORITY		(-1)
 #define QUEUE_STACK_SIZE	512
@@ -48,7 +51,6 @@ static void async_offset(struct k_work *work)
 {
 	atomic_val_t val;
 	ARG_UNUSED(work);
-	val = ztest_get_return_value();
 	zassert_not_null(cli_cb.status_cb, "status_cb is NULL");
 	zassert_not_null(cli_cb.offset_cb, "offset_cb is NULL");
 	zassert_not_null(err_cb, "err_cb is NULL");
@@ -110,22 +112,44 @@ int uart_dfu_cli_write_send(size_t idx,
 			    const u8_t *const fragment_buf,
 			    size_t fragment_size)
 {
-	return ztest_get_return_value();
+	ztest_check_expected_value(fragment_buf);
+	ztest_check_expected_value(fragment_size);
+	int err = ztest_get_return_value();
+	if (err == 0) {
+		k_work_submit_to_queue(&work_queue, &status_work);
+		return 0;
+	}
+	return err;
 }
 
 int uart_dfu_cli_offset_send(size_t idx)
 {
-	return ztest_get_return_value();
+	int err = ztest_get_return_value();
+	if (err == 0) {
+		k_work_submit_to_queue(&work_queue, &offset_work);
+		return 0;
+	}
+	return err;
 }
 
 int uart_dfu_cli_done_send(size_t idx, bool successful)
 {
-	return ztest_get_return_value();
+	ztest_check_expected_value(successful);
+	int err = ztest_get_return_value();
+	if (err == 0) {
+		k_work_submit_to_queue(&work_queue, &status_work);
+		return 0;
+	}
+	return err;
 }
 
 int uart_dfu_cli_stop(size_t idx)
 {
-	return ztest_get_return_value();
+	int err = ztest_get_return_value();
+	if (err == -EINPROGRESS) {
+		k_work_submit_to_queue(&work_queue, &status_work);
+	}
+	return err;
 }
 
 static void dfu_target_callback(enum dfu_target_evt_id evt_id)
@@ -133,27 +157,29 @@ static void dfu_target_callback(enum dfu_target_evt_id evt_id)
 	ztest_check_expected_value(evt_id);
 }
 
-static void state_reset(void)
+static void state_reset(bool first)
 {
-	memset(&cli_cb, 0, sizeof(cli_cb));
-	err_cb = NULL;
-	cli_ctx = NULL;
-
+	if (first) {
+		memset(&cli_cb, 0, sizeof(cli_cb));
+		err_cb = NULL;
+		cli_ctx = NULL;
+	}
 	error_ret = 0;
 	status_ret = 0;
 	offset_ret = 0;
 }
 
-static void test_init_normal(size_t file_size)
+static void session_init(bool first)
 {
 	int err;
+	size_t file_size = TEST_FILE_SIZE;
 	size_t file_size_exp = file_size - UART_DFU_HEADER_LENGTH;
 
-	state_reset();
-	ztest_returns_value(uart_dfu_cli_init, 0);
-	ztest_returns_value(uart_dfu_cli_bind, 0);
-
-	/* Test normal operation */
+	state_reset(first);
+	if (first) {
+		ztest_returns_value(uart_dfu_cli_init, 0);
+		ztest_returns_value(uart_dfu_cli_bind, 0);
+	}	
 	ztest_expect_value(uart_dfu_cli_init_send, file_size, file_size_exp);
 	ztest_returns_value(uart_dfu_cli_init_send, 0);
 	atomic_set(&status_ret, 0);
@@ -161,23 +187,26 @@ static void test_init_normal(size_t file_size)
 	zassert_equal(err, 0, "Invalid init value: %d", err);
 }
 
+static void session_done(void)
+{
+	int err;
+
+	ztest_expect_value(uart_dfu_cli_done_send, successful, true);
+	ztest_returns_value(uart_dfu_cli_done_send, 0);
+	ztest_returns_value(uart_dfu_cli_stop, 0);
+	atomic_set(&status_ret, 0);
+	err = dfu_target_uart_done(true);
+	zassert_equal(err, 0, "Done error: %d", err);
+} 
+
 static void test_init(void)
 {
 	int err;
-	size_t file_size = 150 * 1024;
+	size_t file_size = TEST_FILE_SIZE;
 	size_t file_size_exp = file_size - UART_DFU_HEADER_LENGTH;
 
-	state_reset();
-	ztest_returns_value(uart_dfu_cli_init, 0);
-	ztest_returns_value(uart_dfu_cli_bind, 0);
+	session_init(true);
 
-	/* Test normal operation */
-	ztest_expect_value(uart_dfu_cli_init_send, file_size, file_size_exp);
-	ztest_returns_value(uart_dfu_cli_init_send, 0);
-	atomic_set(&status_ret, 0);
-	err = dfu_target_uart_init(file_size, dfu_target_callback);
-	zassert_equal(err, 0, "Invalid init value: %d", err);
-	
 	/* Test API error */
 	ztest_expect_value(uart_dfu_cli_init_send, file_size, file_size_exp);
 	ztest_returns_value(uart_dfu_cli_init_send, -EINVAL);
@@ -190,19 +219,115 @@ static void test_init(void)
 	atomic_set(&error_ret, -ETIMEDOUT);
 	err = dfu_target_uart_init(file_size, dfu_target_callback);
 	zassert_equal(err, -ETIMEDOUT, "Invalid init value: %d", err);
+
+	session_done();
 }
 
 static void test_offset_get(void)
 {
-	
+	int err;
+	size_t offset = 0;
+	size_t offset_exp = TEST_FILE_SIZE / 2;
+
+	session_init(false);
+
+	/* Test offset */
+	ztest_returns_value(uart_dfu_cli_offset_send, 0);
+	atomic_set(&offset_ret, offset_exp);
+	atomic_set(&status_ret, 0);
+	err = dfu_target_uart_offset_get(&offset);
+	zassert_equal(err, 0, "Error: %d", err);
+	zassert_equal(offset, offset_exp, "Unexpected offset");
+
+	/* Test API error */
+	ztest_returns_value(uart_dfu_cli_offset_send, -EINVAL);
+	err = dfu_target_uart_offset_get(&offset);
+	zassert_equal(err, -EINVAL, "Unexpected error %d", err);
+
+	/* Test protocol error */
+	ztest_returns_value(uart_dfu_cli_offset_send, 0);
+	atomic_set(&error_ret, -ETIMEDOUT);
+	err = dfu_target_uart_offset_get(&offset);
+	zassert_equal(err, -ETIMEDOUT, "Unexpected error: %d", err);
+
+	session_done();
 }
 
 static void test_write(void)
 {
+	int err;
+	u8_t buf[1024];
+	size_t len = sizeof(buf);
+
+	session_init(false);
+
+	/* Test short write with no data */
+	err = dfu_target_uart_write(buf, UART_DFU_HEADER_LENGTH);
+	zassert_equal(err, 0, "Error: %d", err);
+
+	session_done();
+	session_init(false);
+
+	/* Test first write */
+	ztest_expect_value(uart_dfu_cli_write_send, fragment_buf,
+		&buf[UART_DFU_HEADER_LENGTH]);
+	ztest_expect_value(uart_dfu_cli_write_send, fragment_size,
+		len - UART_DFU_HEADER_LENGTH);
+	ztest_returns_value(uart_dfu_cli_write_send, 0);
+	atomic_set(&status_ret, 0);
+	err = dfu_target_uart_write(buf, len);
+	zassert_equal(err, 0, "Error: %d", err);
+
+	/* Test second write */
+	ztest_expect_value(uart_dfu_cli_write_send, fragment_buf, buf);
+	ztest_expect_value(uart_dfu_cli_write_send, fragment_size, len);
+	ztest_returns_value(uart_dfu_cli_write_send, 0);
+	atomic_set(&status_ret, 0);
+	err = dfu_target_uart_write(buf, len);
+	zassert_equal(err, 0, "Error: %d", err);
+
+	/* Test API error */
+	ztest_expect_value(uart_dfu_cli_write_send, fragment_buf, buf);
+	ztest_expect_value(uart_dfu_cli_write_send, fragment_size, len);
+	ztest_returns_value(uart_dfu_cli_write_send, -EINVAL);
+	err = dfu_target_uart_write(buf, len);
+	zassert_equal(err, -EINVAL, "Error: %d", err);
+
+	/* Test protocol error */
+	ztest_expect_value(uart_dfu_cli_write_send, fragment_buf, buf);
+	ztest_expect_value(uart_dfu_cli_write_send, fragment_size, len);
+	ztest_returns_value(uart_dfu_cli_write_send, 0);
+	atomic_set(&status_ret, -ETIMEDOUT);
+	err = dfu_target_uart_write(buf, len);
+	zassert_equal(err, -ETIMEDOUT, "Error: %d", err);
+
+	session_done();
 }
 
 static void test_done(void)
 {
+	int err;
+
+	session_init(false);
+
+	/* Test done */
+	ztest_expect_value(uart_dfu_cli_done_send, successful, true);
+	ztest_returns_value(uart_dfu_cli_done_send, 0);
+	ztest_returns_value(uart_dfu_cli_stop, 0);
+	atomic_set(&status_ret, 0);
+	err = dfu_target_uart_done(true);
+	zassert_equal(err, 0, "Done error: %d", err);
+
+	session_init(false);
+	
+	/* Test in progress */
+	ztest_expect_value(uart_dfu_cli_done_send, successful, true);
+	ztest_returns_value(uart_dfu_cli_done_send, 0);
+	ztest_returns_value(uart_dfu_cli_stop, -EINPROGRESS);
+	atomic_set(&error_ret, -ECANCELED);
+	atomic_set(&status_ret, 0);
+	err = dfu_target_uart_done(true);
+	zassert_equal(err, 0, "Done error: %d", err);
 }
 
 static void work_init(void)
@@ -221,9 +346,9 @@ void test_main(void)
 
 	ztest_test_suite(lib_dfu_target_uart_test,
 		ztest_1cpu_unit_test(test_init),
-		ztest_unit_test(test_offset_get),
-		ztest_unit_test(test_write),
-		ztest_unit_test(test_done)
+		ztest_1cpu_unit_test(test_offset_get),
+		ztest_1cpu_unit_test(test_write),
+		ztest_1cpu_unit_test(test_done)
 	);
 
 	ztest_run_test_suite(lib_dfu_target_uart_test);
