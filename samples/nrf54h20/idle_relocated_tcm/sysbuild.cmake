40a555bd01fd28d9400780139301fa45c252ec52
Jonathan Nilsen <jonathan.nilsen@nordicsemi.no>
2026-03-18 16:16:55 +0100

    Demo how PERIPHCONF entries can be moved to the radio_loader image

---
+++ samples/nrf54h20/idle_relocated_tcm/sysbuild.cmake
@@ -0,0 +1,10 @@
+# Ensure that remote edt.pickle is available when radio_loader CMake runs
+sysbuild_add_dependencies(CONFIGURE radio_loader remote)
+add_dependencies(radio_loader remote)
+
+# CONFIG_NCS_MCUBOOT_PERIPHCONF_TLV is set to y from nrf/sysbuild/CMakeLists.txt
+# and must be disabled explicitly. Since the sysbuild generated .config takes
+# priority over values from prj.conf this is done via Image configuration script.
+set_property(TARGET remote APPEND PROPERTY IMAGE_CONF_SCRIPT
+  ${APP_DIR}/sysbuild_disable_periphconf_tlv.cmake
+)
---
+++ samples/nrf54h20/idle_relocated_tcm/sysbuild_disable_periphconf_tlv.cmake
@@ -0,0 +1,2 @@
+# Don't generate PERIPHCONF TLV for remote
+set_config_bool(remote CONFIG_NCS_MCUBOOT_PERIPHCONF_TLV n)
